88458cc365025d13dfca973a4ec340ca63131530
Sara Borgen- Greenfeld <154741733+saraborgen1@users.noreply.github.com>
2025-06-04 10:24:58 +0300

    Update main_program_1.sql

--- phase4/main_program_1.sql
+++ phase4/main_program_1.sql
@@ -1,12 +1,17 @@
 DO $$
 DECLARE
-  rec RECORD;
+    rec RECORD;
 BEGIN
-  -- מעבר על כל השחקנים והנקודות שלהם דרך הפונקציה שמחזירה טבלה
-  FOR rec IN
-    SELECT * FROM get_all_players_points()
-  LOOP
-    RAISE NOTICE 'Player ID: %, Name: % %, Total Points: %',
-      rec.playerid, rec.playerfirstname, rec.playerlastname, rec.total_points;
-  END LOOP;
+    -- שלב 1: הפעלת הפרוצדורה לעדכון ניסיון המאמנים
+    CALL increase_experience_of_coaches();
+
+    -- שלב 2: הדפסת נקודות כל השחקנים לפי ספורט
+    RAISE NOTICE 'רשימת נקודות לשחקנים:';
+    
+    FOR rec IN SELECT * FROM get_all_players_points()
+    LOOP
+        RAISE NOTICE 'ID: %, % %, Sport: %, Points: %',
+            rec.playerid, rec.playerfirstname, rec.playerlastname,
+            rec.sporttype, rec.total_points;
+    END LOOP;
 END $$;
